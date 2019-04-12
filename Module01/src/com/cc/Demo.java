b81324a4a4c7f27da07d37e947f88ed928c90a75
admin <369249324@qq.com>
2019-04-12 11:36:31 +0800

    first

---
+++ Module01/src/com/cc/Demo.java
@@ -0,0 +1,18 @@
+package com.cc;
+
+public class Demo {
+
+    public static void main(String[] args){
+          System.out.println("hello word!");
+
+          //String
+    }
+    //这是单行注释
+    public void method(){
+      
+
+    }
+
+
+
+}
